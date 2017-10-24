ca91ff5ab27f40ebf605a7f149420baae1d2ba66
menghou <hmm8121451@163.com>
2017-10-24 22:13:13 +0800

    add median of two sorted arrays

--- median_of_two_sorted_arrays.cpp
+++ median_of_two_sorted_arrays.cpp
@@ -1,7 +1,6 @@
 //
 // Created by houmeng on 17-10-24.
 //
-//TODO There are two sorted arrays nums1 and nums2 of size m and n respectively.
 /*
 Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
 
@@ -17,4 +16,45 @@ nums2 = [3, 4]
 The median is (2 + 3)/2 = 2.5
 
  */
+#include <vector>
+#include <iostream>
+using std::vector;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        int total = m + n;
+        if (total & 1) {
+            return findKthVale(nums1, nums2, total/2-1);
+        } else {
+            return (findKthVale(nums1, nums2, total/2) +
+                   findKthVale(nums1, nums2, total/2 - 1))/2;
+        }
+    }
+};
+
+double findKthVale(vector<int>& nums1, vector<int>& nums2, int k) {
+    size_t m = nums1.size(), n = nums2.size();
+    if (m > n) {
+        return findKthVale(nums2, nums1, k);
+    }
+    if (m == 0) {
+        return nums2[k-1];
+    }
+
+    int pl = min(k/2, m), pr = k - pl;
+
+    if (nums1[pl - 1] < nums2[pr - 1] ) {
+        vector<int> newNums1(nums1.begin() + pl, nums1.end());
+        return findKthVale(newNums1, nums2, k - pl);
+    } else if (nums1[p1 - 1] > nums2[pr - 1]) {
+        vector<int> newNums2(nums2.begin() + pr, nums2.end());
+        return findKthVale(nums1, newNums2, k - pr);
+    } else {
+        return nums1[pl-1];
+    }
+}
+
+
 
